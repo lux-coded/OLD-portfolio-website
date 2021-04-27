bed55435364cbb78d8321de6535c1305ba089b3f
lux-coded <selakluka@gmail.com>
2021-04-27 16:53:06 +0200

    Color style testing.

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -7,6 +7,18 @@ class Work extends React.Component {
     return (
       <section id='work-section'>
         <h1>Work</h1>
+        <div className='project-card-container'>
+          <article className='project-card'>
+            <h2>SnackTime</h2>
+            <button className='project-card-button'>View</button>
+            <button className='project-card-button'>Code</button>
+          </article>
+          <article className='project-card'>
+            <h2>TeamUp</h2>
+            <button className='project-card-button'>View</button>
+            <button className='project-card-button'>Code</button>
+          </article>
+        </div>
       </section>
     );
   }
