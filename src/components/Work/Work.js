4ad19ef5ee8767b60987dfb15613f58dcb402b25
lux-coded <selakluka@gmail.com>
2021-06-04 23:19:25 +0200

    Latest project uploaded.

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -27,7 +27,7 @@ class Work extends React.Component {
             </div>
             <div className='project-image-container'>
               <a href='https://snacktime.netlify.app/'>
-                <img src='images/snacktime-thumbnail.png' className='project-image' alt='project_thumbnail' />
+                <img src='https://d33wubrfki0l68.cloudfront.net/6096f4db9d5adf00073362c5/screenshot_2021-05-08-20-30-59-0000.png' className='project-image' alt='project_thumbnail' />
               </a>
             </div>
             <h4>Built with:</h4>
@@ -88,15 +88,15 @@ class Work extends React.Component {
           </article>
           <article className='project-card'>
             <div className='project-header'>
-              <h2>Bruh</h2>
+              <h2>eStore</h2>
               <div className='project-button-container'>
                 <button className='project-card-button'>
-                  <a href='https://snacktime.netlify.app/'>
+                  <a href='https://e-store-commerce.netlify.app/'>
                     View
                   </a>
                 </button>
                 <button className='project-card-button'>
-                  <a href='https://github.com/lux-coded/snack-time'>
+                  <a href='https://github.com/lux-coded/e-store'>
                     Code
                   </a>
                   {/* <ion-icon name="code-outline" size='large'></ion-icon> */}
@@ -104,12 +104,21 @@ class Work extends React.Component {
               </div>
             </div>
             <div className='project-image-container'>
-              <img src='' className='project-image' alt='project_thumbnail'></img>
+              <img src='https://d33wubrfki0l68.cloudfront.net/60ba97900bf73210925fc925/screenshot_2021-06-04-21-15-42-0000.png' className='project-image' alt='project_thumbnail'></img>
             </div>
             <h4>Built with:</h4>
             <br></br>
-            <h5>Bruh, bruh, bruh, bruh, bruh, bruh</h5>
-            <p>Bruh.</p>
+            <div className='tech-tag-container'>
+              <p className='tech-tag'>HTML</p>
+              <p className='tech-tag'>CSS</p>
+              <p className='tech-tag'>Sass</p>
+              <p className='tech-tag'>Javascript</p>
+              <p className='tech-tag'>React</p>
+              <p className='tech-tag'>Stripe</p>
+              <p className='tech-tag'>Commerce.js</p>
+            </div>
+            <br></br>
+            <p>E-Commerce store integrated with Stripe for technology purchases.</p>
           </article>
         </div>
       </section>
